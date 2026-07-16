0fdf9b2324a39e892dc9af2ca6ea04c7a99e297c
xiaohforensic-tech <xiaoh.forensic@gmail.com>
2026-07-16 22:58:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="joint-loss-v6";
-const ASSETS=["./","./index.html","./styles.css?v=1.4.0","./app.js?v=1.4.0","./src/joint-data.js?v=1.4.0","./src/calculator.js?v=1.4.0","./manifest.webmanifest","./assets/icon.svg"];
+const CACHE="joint-loss-v8";
+const ASSETS=["./","./index.html","./styles.css?v=1.6.0","./app.js?v=1.6.0","./src/joint-data.js?v=1.6.0","./src/calculator.js?v=1.6.0","./manifest.webmanifest","./assets/icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
