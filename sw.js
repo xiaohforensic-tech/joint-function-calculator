41b0de2e00e92337244a6238fbb925e48e939ca0
xiaohforensic-tech <xiaoh.forensic@gmail.com>
2026-07-16 11:55:04 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE="joint-loss-v5";
+const ASSETS=["./","./index.html","./styles.css?v=1.2.0","./app.js?v=1.3.0","./src/joint-data.js?v=1.3.0","./src/calculator.js?v=1.3.0","./manifest.webmanifest","./assets/icon.svg"];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone(); caches.open(CACHE).then(cache=>cache.put(event.request,copy)); return response;
+  }).catch(()=>caches.match(event.request)));
+});
